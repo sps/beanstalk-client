f18c8b08265562d390134add2e71c44acc75fe32
cyn0n <cyn0n@spicetrader.r3sty.com>
2009-02-08 08:51:57 +0000

    get/put test; setup/teardown need to be re-worked it appears

--- TestBeanstalk.java
+++ TestBeanstalk.java
@@ -5,11 +5,29 @@ public class TestBeanstalk {
 
   private Beanstalk bean;
 
+  // setups and teardowns are not quite working here yet..
   protected void setUp() {
     bean = new Beanstalk();
   }
 
   protected void tearDown() {
+    bean.close();
+  }
+
+  @Test
+  public void testputJob() {
+    bean = new Beanstalk();
+    bean.putJob("test");
+    bean.close();
+  }
+
+  @Test
+  public void testgetJob() {
+    bean = new Beanstalk();
+    Job job = new Job();
+    job = bean.getJob();
+    bean.getJob();
+    assertEquals("test", job.msg);
     bean.close();
   }
 
